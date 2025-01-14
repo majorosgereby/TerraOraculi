b954f303580c39fd9a28222c7ce69b674c79ae5e
majorosgereby <25921022+majorosgereby@users.noreply.github.com>
2025-01-14 14:50:25 +0100

    add self authorized ssl to server

--- app/backend/server.js
+++ app/backend/server.js
@@ -2,13 +2,33 @@
 import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import https from "https";
+import fs from "fs";
+import dotenv from "dotenv";
+import morgan from "morgan";
 
 // Get __dirname equivalent in ES modules
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+// HTTPS config
+// https://dev.to/fredabod/building-an-express-app-with-an-https-server-2mbj
+// https://chatgpt.com/share/67866246-2df8-8013-b0c4-e08c5c0f8965
+dotenv.config();
+
 // Create an Express app
 const app = express();
+
+// middlewear for https
+app.use(morgan("dev"));
+
+// Read SSL certificate and key files
+const options = {
+    key: fs.readFileSync(path.join(__dirname, "keys", "express-example-key.pem")),
+    cert: fs.readFileSync(path.join(__dirname, "keys", "express-example.pem")),
+};
+
+// ENDPOINTS
 
 // Serve static HTML files from '../content/pages' folder
 app.use(express.static(path.join(__dirname, '..', 'content', 'pages')));
@@ -21,6 +41,9 @@ app.use('/img', express.static(path.join(__dirname, '..', 'content', 'assets')))
 
 // Start the server
 const PORT = process.env.PORT || 8128;
-app.listen(PORT, () => {
+
+const server = https.createServer(options, app);
+
+server.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });

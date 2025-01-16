0d4b721247b7a7340f1ffd3b6b61127c70d94bee
majorosgereby <25921022+majorosgereby@users.noreply.github.com>
2025-01-16 13:11:13 +0100

    add docker tag and push to terraform

--- deployment/main.tf
+++ deployment/main.tf
@@ -27,3 +27,15 @@ resource "aws_ecr_repository" "express_image_repo" {
   }
 }
 
+# pushing the docker image into the container
+resource "null_resource" "docker_push" {
+  provisioner "local-exec" {
+    command = <<EOT
+      aws ecr get-login-password --region eu-west-2 | docker login --username AWS --password-stdin $(aws ecr describe-repositories --repository-names practice/express-example --region eu-west-2 | jq -r '.repositories[0].repositoryUri')
+      docker tag express-example:latest $(aws ecr describe-repositories --repository-names practice/express-example --region eu-west-2 | jq -r '.repositories[0].repositoryUri'):latest
+      docker push $(aws ecr describe-repositories --repository-names practice/express-example --region eu-west-2 | jq -r '.repositories[0].repositoryUri'):latest
+    EOT
+  }
+
+  depends_on = [aws_ecr_repository.express_image_repo]
+}

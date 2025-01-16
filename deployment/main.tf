a8e11cb687ed6d39ea199447d91046c012cb5e59
majorosgereby <25921022+majorosgereby@users.noreply.github.com>
2025-01-16 13:07:46 +0100

    terraform initializtaion and ECR creation

---
+++ deployment/main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "eu-west-2"
+}
+
+# ECR for storing the docker image
+resource "aws_ecr_repository" "express_image_repo" {
+  name                 = "practice/express-example"
+  image_tag_mutability = "MUTABLE"
+
+  encryption_configuration {
+    encryption_type = "AES256"
+  }
+
+  image_scanning_configuration {
+    scan_on_push = false
+  }
+}
+
